6099a15572a1cd5cc292d83645462eb24e1e0db3
Yasser Bazzi <yassermbb@gmail.com>
2021-03-27 15:04:07 -0300

    Cleaner solution

--- src/decompressor/CompressorFinder.java
+++ src/decompressor/CompressorFinder.java
@@ -2,63 +2,42 @@ package decompressor;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.LinkOption;
 import java.nio.file.Path;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import org.apache.commons.compress.compressors.bzip2.BZip2Utils;
 
 public final class CompressorFinder {
-	public static final Logger LOGGER = Logger.getLogger("Compressor");
+	public static final Logger LOGGER = Logger.getLogger(CompressorFinder.class.getSimpleName());
 
-	private static final Path COMPRESSEDFILESLOCATION = Path.of(System.getProperty("compressor","./"));
-	private static final Path DECOMPRESSEDFILESLOCATION = Path.of(System.getProperty("decompressor","./"));
+	private static final Path COMPRESSEDFILESLOCATION = Path.of(System.getProperty("compressor", "./"));
+	private static final Path DECOMPRESSEDFILESLOCATION = Path.of(System.getProperty("decompressor", "./"));
 
 	//
 	public static void main(final String[] args) {
-		System.out.println("Path to compressed files: " + COMPRESSEDFILESLOCATION);
-		System.out.println("Path to decompress files: " + DECOMPRESSEDFILESLOCATION);
+		LOGGER.log(Level.INFO,"Path to compressed files: " + COMPRESSEDFILESLOCATION);
+		LOGGER.log(Level.INFO,"Path to decompress files: " + DECOMPRESSEDFILESLOCATION);
+		checkNewItems(COMPRESSEDFILESLOCATION);
+		LOGGER.log(Level.WARNING, "Done decompressing files");
 		
-		final var files = new HashSet<Path>();
-		
-		while(true)
-		{
-			checkNewItems(COMPRESSEDFILESLOCATION,files);
-			try {
-				TimeUnit.SECONDS.sleep(5);
-			} catch (InterruptedException e) {
-				//SHOULD NEVER REACH HERE
-				LOGGER.severe(() -> "Error on thread " + Arrays.toString(e.getStackTrace()));
-			}
-			System.gc();
-		}		
 	}
-	private static final void checkNewItems(final Path pathToItems, final Set<Path> files){
-		try(final var file = Files.newDirectoryStream(pathToItems,"*.bz2"))
-		{
-			file.forEach(path -> computeNewFile(files, path));			
+
+	private static final void checkNewItems(final Path pathToItems) {
+		try (final var file = Files.newDirectoryStream(pathToItems, "*.bz2")) {
+			file.forEach(path -> computeNewFile(path));
 		} catch (IOException e) {
-			//SHOULD NEVER REACH HERE
-			LOGGER.severe(() -> "Error searching files, message: " + Arrays.toString(e.getStackTrace()));
-		}				
-	}	
-	private static final void computeNewFile(final Set<Path> files,final Path path) {		
-		if(files.add(path))
-		{
-			final var filename = BZip2Utils.getUncompressedFilename(path.getFileName().toString());
-			System.out.println("New file added to the list: " + filename);			
-			try(final var fileStream = Files.list(DECOMPRESSEDFILESLOCATION)) {
-				if(fileStream.noneMatch(e -> e.getFileName().toString().equals(filename))) {
-				DecompressorUtil.decompress(path, DECOMPRESSEDFILESLOCATION,filename);
-				}
-			} catch (IOException e) {
-				//SHOULD NEVER REACH HERE
-				LOGGER.severe(() -> "Error walking files on: " + path + " Message: " + Arrays.toString(e.getStackTrace()));
-			}
+			// SHOULD NEVER REACH HERE
+			LOGGER.log(Level.SEVERE, e, e::getMessage);
 		}
 	}
-	
+
+	private static final void computeNewFile(final Path path) {
+		final var filename = BZip2Utils.getUncompressedFilename(path.getFileName().toString());
+		if (Files.notExists(DECOMPRESSEDFILESLOCATION.resolve(filename))) {
+			DecompressorUtil.decompress(path, DECOMPRESSEDFILESLOCATION, filename);
+		}
+	}
+
 }
--- src/decompressor/DecompressorUtil.java
+++ src/decompressor/DecompressorUtil.java
@@ -3,33 +3,40 @@ package decompressor;
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.IOException;
+import java.nio.channels.Channel;
+import java.nio.channels.Channels;
+import java.nio.channels.FileChannel;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Arrays;
 import java.util.Objects;
 import java.util.function.Supplier;
+import java.util.logging.Level;
 
 import org.apache.commons.compress.compressors.CompressorException;
 import org.apache.commons.compress.compressors.CompressorStreamFactory;
-import org.apache.commons.compress.utils.IOUtils;
 
 public final class DecompressorUtil {
-	private DecompressorUtil() {}
-	public static final void decompress(final Path fileToDecompress,final Path pathToDecompressTo,final String filename) {
-		final Supplier<String> message = () -> "Path cannot be null"; 
+	private DecompressorUtil() {
+	}
+
+	public static final void decompress(final Path fileToDecompress, final Path pathToDecompressTo,
+			final String filename) {
+		final Supplier<String> message = () -> "Path cannot be null";
 		Objects.requireNonNull(fileToDecompress, message);
 		Objects.requireNonNull(pathToDecompressTo, message);
 		Objects.requireNonNull(filename, message);
-		
-		final var finalPath = pathToDecompressTo.resolve(filename);
-		try(final var is = new BufferedInputStream(Files.newInputStream(fileToDecompress)); 
-			final var in = new CompressorStreamFactory().createCompressorInputStream(CompressorStreamFactory.BZIP2,is);
-			final var out = new BufferedOutputStream(Files.newOutputStream(finalPath)))
-		{
-			IOUtils.copy(in, out);
-			System.out.println("File: " + filename + " was extracted to: " + pathToDecompressTo);
+
+		final Path finalPath = pathToDecompressTo.resolve(filename);
+		try (final var is = new BufferedInputStream(Files.newInputStream(fileToDecompress));
+				final var in = new CompressorStreamFactory().createCompressorInputStream(CompressorStreamFactory.BZIP2,
+						is);
+				final var out = new BufferedOutputStream(Files.newOutputStream(finalPath));) {
+			in.transferTo(out);
+
+			CompressorFinder.LOGGER.log(Level.INFO, () -> "File: " + filename + " was extracted to: " + pathToDecompressTo);
+			
 		} catch (IOException | CompressorException e) {
-			CompressorFinder.LOGGER.severe(() -> "Error extracting file " + Arrays.toString(e.getStackTrace()));
+			CompressorFinder.LOGGER.log(Level.SEVERE, e, e::getMessage);
 		}
-	}	
+	}
 }
